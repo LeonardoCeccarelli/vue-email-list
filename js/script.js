ddfa995bb0f87d0bfb93f5d00bdc2ae3c25c1633
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-09 14:41:23 +0100

    add call api for received email from server

--- js/script.js
+++ js/script.js
@@ -5,9 +5,23 @@ Vue.config.devtools = true;
 new Vue({
     el: "#root",
     data: {
-
+        emailList: [],
+        counterApiCalls: 0,
     },
-    methods: {
 
+    methods: {
+        getApiEmailList(array) {
+
+            const url = "https://flynn.boolean.careers/exercises/api/random/mail"
+
+            for (let i = 0; i < 10; i++) {
+
+                axios.get(url).then((ajaxResponse) => {
+                    array.push(ajaxResponse.data.response)
+                })
+            }
+
+            console.log(this.emailList);
+        }
     },
 })
