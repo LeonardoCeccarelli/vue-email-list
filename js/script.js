4d80caa1a391fce4efbab16cf049946e842c53b0
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-09 17:26:01 +0100

    using if into .then i remove v-if in html

--- js/script.js
+++ js/script.js
@@ -6,25 +6,31 @@ new Vue({
     el: "#root",
     data: {
         emailList: [],
-        counterApiCalls: 0,
     },
 
     methods: {
         getApiEmailList() {
 
-            this.counterApiCalls = 0
-
             this.emailList = []
+
+            let counterApiCalls = 0
+
+            const verifiedArray = []
 
             const url = "https://flynn.boolean.careers/exercises/api/random/mail"
 
             for (let i = 0; i < 10; i++) {
 
-                this.counterApiCalls++
-
                 axios.get(url).then((ajaxResponse) => {
-                    this.emailList.push(ajaxResponse.data.response)
+                    counterApiCalls++
+                    verifiedArray.push(ajaxResponse.data.response)
+
+                    if (counterApiCalls === 10) {
+                        this.emailList = verifiedArray
+                    }
                 })
+
+
             }
         }
     },
